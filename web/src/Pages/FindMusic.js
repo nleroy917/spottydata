36c2c50ac7b40868a22749bea8994765221e91c7
Nathan LeRoy <NLeRoy917@gmail.com>
2020-11-16 18:38:14 -0500

    work on parameter stuff

--- web/src/Pages/FindMusic.js
+++ web/src/Pages/FindMusic.js
@@ -7,15 +7,33 @@ import {
     Container,
     TextField,
     CircularProgress,
-    makeStyles
+    makeStyles,
+    IconButton,
+    Slider
 } from '@material-ui/core';
-
+import ClearIcon from '@material-ui/icons/Clear';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-
-import Layout from '../Components/Layout';
 
 const querystring = require('querystring');
 const URL_BASE = process.env.REACT_APP_API_URL
+
+const muiTheme = createMuiTheme({
+    overrides:{
+      MuiSlider: {
+        thumb:{
+        color: "white",
+        },
+        track: {
+          color: '#007bff'
+        },
+        rail: {
+          color: 'black'
+        }
+      }
+  }
+  });
 
 const PageHeaderWrapper = styled.div`
     display: flex;
@@ -45,6 +63,26 @@ const SeedTypeWrapper = styled.div`
     gap: 30px;
 `
 
+const ParameterForm = styled.div`
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: 100%;
+    gap: 40px;
+`
+
+const FormSection = styled.div`
+    padding: 5px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    gap: 50px;
+`
+
 const Select = styled.select`
     padding: 15px;
     color: white;
@@ -55,6 +93,32 @@ const Select = styled.select`
     &:focus {
         outline: none;
     }
+`
+
+const GoalSelect = styled.select`
+    margin: 5px;
+    margin-right: 15px;
+    padding: 5px;
+    font-size: 12px;
+    color: white;
+    border: white solid 1px;
+    border-radius: 0px;
+    background: none;
+    height: 100%;
+    &:focus {
+        outline: none;
+    }
+`
+
+const Parameter = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`
+
+const ParameterTitle = styled.h4`
+    font-size: 24px;
+    font-weight: 500px;
 `
 
 const useStyles = makeStyles({
@@ -100,6 +164,8 @@ const FindMusic = () => {
     const [searchResults, setSearchResults] = useState([]);
     const [loading, setLoading] = useState(false);
     const [accessToken, setAccessToken] = useState(null)
+    const [attributes, setAttributes] = useState([]);
+    const DEFAULT_VAL = 0.5;
 
     const clientCredentialsFlow = async () => {
         let hdrs = {
@@ -123,7 +189,6 @@ const FindMusic = () => {
           setQuery(query)
           setLoading(true)
           searchSeeds(query)
-          console.log(query)
         },
         // delay in ms
         500
@@ -136,17 +201,22 @@ const FindMusic = () => {
     const handleSeedTypeChange = (e) => {
         setSearchResults([])
         setSeedType(e.target.value)
-        console.log(e.target.value)
+        //console.log(e.target.value)
     }
 
     const searchSeeds = async (query) => {
         if(seedType === 'genre'){
-            setLoading(false)
-            setSearchResults([
-                {name: 'country'},
-                {name: 'rap'},
-                {name: 'rock'}
-            ])
+            let hdrs = {
+                Authorization: `Bearer ${accessToken}`
+            }
+            let res = await axios.get(`https://api.spotify.com/v1/recommendations/available-genre-seeds`, {headers: hdrs})
+            if(res.status === 200){
+                let data = res.data
+                setLoading(false)
+                setSearchResults(data.genres.map(genre => {
+                    return {name: genre}
+                }))
+            }
         } else{
         if(!query){
             setLoading(false)
@@ -164,9 +234,9 @@ const FindMusic = () => {
     }}
     }
     useEffect(() => {
-        clientCredentialsFlow()
-        console.log(seed)
-    }, [seed])
+        if(!accessToken){clientCredentialsFlow()}
+        //console.log(attributes)
+    }, [])
     return (
         <>
          <Container>
@@ -224,6 +294,89 @@ const FindMusic = () => {
             )}
            />
            </SeedTypeWrapper>
+           <PageHeaderWrapper>
+         <PageSubHeader>
+             2. Tweak parameters:
+         </PageSubHeader>
+         </PageHeaderWrapper>
+         <ThemeProvider theme={muiTheme}>
+           <ParameterForm>
+               <FormSection>
+                   <Parameter>
+                     <ParameterTitle>
+                        Acousticness
+                     </ParameterTitle>
+                     <Slider
+                       style={{width: 250}}
+                       aria-labelledby="acousticness-slider"
+                       min={0}
+                       max={1}
+                       step={0.01}
+                       defaultValue={0.5}
+                       onChange={(e,val)=>{setAttributes({...attributes, acousticness: {...attributes.acousticness, value: val}})}}
+                     />
+                     <span>
+                     <GoalSelect onChange={(e) => setAttributes({...attributes, acousticness: {...attributes.acousticness, goal: e.target.value, value: attributes.acousticness ? attributes.acousticness.value : DEFAULT_VAL}})}>
+                         <option value='target'>Target</option>
+                         <option value='min'>Min</option>
+                         <option value='max'>Max</option>
+                     </GoalSelect>
+                     <b>{attributes.acousticness ? `${attributes.acousticness.value}` : 'None chosen'}</b>
+                     <IconButton style={{margin: '10px', outline: 'none'}} onClick={() => setAttributes({...attributes, acousticness: null})}>
+                         <ClearIcon style={{fill: 'white'}} size="0.5em"/>
+                     </IconButton>
+                     </span>
+                   </Parameter>
+                   <Parameter>
+                     <ParameterTitle>
+                       Danceability
+                     </ParameterTitle>
+                     <Slider
+                       style={{width: 250}}
+                       aria-label="danceability-slider"
+                       min={0}
+                       max={1}
+                       step={0.00000001}
+                       defaultValue={0.5}
+                       onChange={(e,val)=>{setAttributes({...attributes, danceability: {...attributes.danceability, value: val}})}}
+                     />
+                     <span>
+                     <GoalSelect onChange={(e) => setAttributes({...attributes, danceability: {...attributes.danceability, goal: e.target.value, value: attributes.danceability ? attributes.danceability.value : DEFAULT_VAL}})}>
+                         <option value='target'>Target</option>
+                         <option value='min'>Min</option>
+                         <option value='max'>Max</option>
+                     </GoalSelect>
+                     <b>{attributes.danceability ? `${attributes.danceability.value}` : 'None chosen'}</b>
+                     </span>
+                   </Parameter>
+                   <Parameter>
+                    <ParameterTitle>
+                     Energy
+                     </ParameterTitle>
+                     <Slider
+                       style={{width: 250}}
+                       aria-label="danceability-slider"
+                       min={0}
+                       max={1}
+                       step={0.00000001}
+                       defaultValue={0.5}
+                       onChange={(e,val)=>{setAttributes({...attributes, energy: {...attributes.energy, value: val}})}}
+                     />
+                     <span>
+                     <GoalSelect onChange={(e) => setAttributes({...attributes, energy: {...attributes.energy, goal: e.target.value, value: attributes.energy ? attributes.energy.value : DEFAULT_VAL}})}>
+                         <option value='target'>Target</option>
+                         <option value='min'>Min</option>
+                         <option value='max'>Max</option>
+                     </GoalSelect>
+                     <b>{attributes.energy ? `${attributes.energy.value}` : 'None chosen'}</b>
+                     </span>
+                   </Parameter>
+               </FormSection>
+               <FormSection>
+                   <div>Here</div><div>Here</div><div>Here</div>
+               </FormSection>
+           </ParameterForm>
+           </ThemeProvider>
           </Container>
         </>
     )
